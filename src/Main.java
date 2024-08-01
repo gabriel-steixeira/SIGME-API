09e9141a06af75a59857a3082132d2473be80338
Gabriel Teixeira <87240166+gabriel-steixeira@users.noreply.github.com>
2024-08-01 23:49:52 +0000

    Incluindo Conexao ao Banco de Dados

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,10 @@ import sigme.controllers.ProdutoController;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Date dataValidade = new Date(2024, 5, 16);
+        Date dataValidade = new Date(2025, 5, 16);
         Date dataLote = new Date(2024, 5, 16);
-        Date dataCadastro = new Date(2024, 5, 16);
-        Date dataAlteracao = new Date(2024, 5, 16);
+        Date dataCadastro = new Date(2024, 5, 21);
+        Date dataAlteracao = new Date(2024, 5, 21);
         Produto produto1 = new Produto(1, "Escova de dentes", "Colgato", "Higiene Pessoal", 7.5, 100, dataValidade, dataLote, "1", "gabriel.teixeira", "gabriel.teixeira", dataCadastro, dataAlteracao, 1);
         Produto produto2 = new Produto(2, "Pasta de dentes", "Colgato", "Higiene Pessoal", 5, 100, dataValidade, dataLote, "1", "gabriel.teixeira", "gabriel.teixeira", dataCadastro, dataAlteracao, 1);
         Produto produto3 = new Produto(3, "Sabonete", "Dovi", "Higiene Pessoal", 3.8, 100, dataValidade, dataLote, "1", "gabriel.teixeira", "gabriel.teixeira", dataCadastro, dataAlteracao, 1);
@@ -19,11 +19,11 @@ public class Main {
         ProdutoController produtoController = new ProdutoController();
         FornecedorController fornecedorController = new FornecedorController();
 
-        Fornecedor fornecedor1 = new Fornecedor(1, "Jhonsom", "Jhonsom & Jhonsom", "123456789/0001-24", "jhonsom@gmail.com", "11987654321", "gabriel.teixeira", "gabriel.teixeira");
-//        fornecedorController.CadastrarFornecedor(fornecedor1);
+        Fornecedor fornecedor1 = new Fornecedor(1, "Colgato", "Colgato", "123456789/0001-24", "colgato@colgato.com", "11987654321", "gabriel.teixeira", "gabriel.teixeira");
+        fornecedorController.CadastrarFornecedor(fornecedor1);
 
-        produtoController.cadastrarProduto(produto1);
-        produtoController.cadastrarProduto(produto2);
-        produtoController.cadastrarProduto(produto3);
+        //produtoController.cadastrarProduto(produto1);
+        //produtoController.cadastrarProduto(produto2);
+        //produtoController.cadastrarProduto(produto3);
     }
 }
--- src/sigme/controllers/ConexaoBanco.java
+++ src/sigme/controllers/ConexaoBanco.java
@@ -8,7 +8,8 @@ public class ConexaoBanco {
 
     public Connection Conectar() {
         try {
-            return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XEPDB1", "APP_SIGME_SIS", "sigme_2024");
+            return DriverManager.getConnection("DB_URL=jdbc:oracle:thin:(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.sa-saopaulo-1.oraclecloud.com))(connect_data=(service_name=g1f69bdd1ef191b_baseatp_medium.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))", "APP_SIGME", "Usr_Sigme_2024");
+            //return DriverManager.getConnection("jdbc:oracle:thin:@adb.sa-saopaulo-1.oraclecloud.com:1522/g1f69bdd1ef191b_baseatp_medium.adb.oraclecloud.com", "APP_SIGME", "Usr_Sigme_2024");
         } catch (SQLException e) {
             System.err.println(e.getMessage());
             return null;
